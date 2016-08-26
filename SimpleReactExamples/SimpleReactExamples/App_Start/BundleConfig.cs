50ffd3b2c2f08296870702eca51121809054e59a
squad <squad@SQUADWUSCHEL>
2016-08-26 19:56:23 +0200

    Projekt neu anagelegt

---
+++ SimpleReactExamples/SimpleReactExamples/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace SimpleReactExamples
+{
+    public class BundleConfig
+    {
+        // Weitere Informationen zu Bundling finden Sie unter "http://go.microsoft.com/fwlink/?LinkId=301862"
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
