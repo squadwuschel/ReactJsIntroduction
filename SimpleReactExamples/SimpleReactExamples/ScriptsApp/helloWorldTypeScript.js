20ac76d15b260a103401a6691499dc6eee09e8b1
squad <squad@SQUADWUSCHEL>
2016-08-28 00:05:05 +0200

    test

--- SimpleReactExamples/SimpleReactExamples/ScriptsApp/helloWorldTypeScript.js
+++ SimpleReactExamples/SimpleReactExamples/ScriptsApp/helloWorldTypeScript.js
@@ -2,7 +2,6 @@
 // A '.tsx' file enables JSX support in the TypeScript compiler, 
 // for more information see the following page on the TypeScript wiki:
 // https://github.com/Microsoft/TypeScript/wiki/JSX
-//http://staxmanade.com/2015/08/playing-with-typescript-and-jsx/
 var Counter = React.createClass({
     add: function () {
         console.log('add 1!');
--- SimpleReactExamples/SimpleReactExamples/ScriptsApp/helloWorldTypeScript.tsx
+++ SimpleReactExamples/SimpleReactExamples/ScriptsApp/helloWorldTypeScript.tsx
@@ -3,9 +3,6 @@
 // for more information see the following page on the TypeScript wiki:
 // https://github.com/Microsoft/TypeScript/wiki/JSX
 
-
-//http://staxmanade.com/2015/08/playing-with-typescript-and-jsx/
-
 var Counter = React.createClass({
     add: () => {
         console.log('add 1!');
